7e7db95051df01ee9659058b170264d1bd1cb057
Val.istar.Guo <Val.istar.Guo@gmail.com>
2017-09-17 22:14:35 +0800

    add webpack-bundle-analyzer

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,17 @@
 import server from './build/webpack.config.server.js';
 import client from './build/webpack.config.client.js';
 import ssr from './build/webpack.config.ssr.js';
+import analyzer from './build/webpack.config.analyzer.js';
+
+const configs = [];
 
 
-export default [server, ssr, client];
+if (process.env.COMPILE_ENV === 'analyzer') {
+  configs.push(analyzer);
+} else {
+  configs.push(server);
+  configs.push(client);
+  configs.push(ssr);
+}
+
+export default configs;
