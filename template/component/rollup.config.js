e08b037ff6a7d863b2a090822ade6bd6bb91f7f6
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-04-18 22:59:14 +0800

    component project support custom some build config

--- template/component/rollup.config.js
+++ template/component/rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs'
+import config from './build.config'
 
 export default [{
   input: 'src/index.js',
@@ -7,19 +9,21 @@ export default [{
     file: 'dist/index.js',
     format: 'cjs',
   },
+  ...config,
 
   plugins: [
     resolve(),
     babel({
       babelrc: false,
       presets: [
-        ["env", {
-          modules: false
-        }],
+        ["env", { modules: false }],
       ],
       plugins: ["transform-object-rest-spread", "external-helpers"],
       // runtimeHelpers: true,
       exclude: 'node_modules/**',
     }),
+    commonjs({
+      include: 'node_modules/**',
+    }),
   ],
-}];
+}]
