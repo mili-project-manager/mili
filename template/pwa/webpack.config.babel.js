2d4ed1d0daf53f28aa03b21f1c2aa548bdfa4dca
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-05-06 22:51:51 +0800

    remove unuseful files

--- template/pwa/webpack.config.babel.js
+++ template/pwa/webpack.config.babel.js
@@ -1,7 +1,6 @@
 import server from './build/webpack.config.server.js'
 import client from './build/webpack.config.client.js'
 import ssr from './build/webpack.config.ssr.js'
-import sw from './build/webpack.config.sw'
 
 const configs = [server, client, ssr]
 
