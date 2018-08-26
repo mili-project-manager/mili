d938b11a6d685869512b705875a8e680c51adddd
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-08-26 23:51:23 +0800

    component template: can custom rollup config

--- template/component/build.config.js
+++ template/component/build.config.js
@@ -1,1 +1,29 @@
-export default {}
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs'
+
+export const plugins = [
+  json(),
+  resolve(),
+  babel({
+    babelrc: false,
+    presets: [
+      ["env", { modules: false }],
+    ],
+    plugins: ["transform-object-rest-spread", "external-helpers"],
+    // runtimeHelpers: true,
+    exclude: 'node_modules/**',
+  }),
+  commonjs({
+    include: 'node_modules/**',
+  }),
+]
+
+export const compiler = config => {
+  const clone = {...config};
+  if (config && config.output && !config.output.format) clone.output.format = 'cjs'
+  if (!config.plugins) clone.plugins = plugins
+
+  return clone
+}
--- template/component/rollup.config.js
+++ template/component/rollup.config.js
@@ -1,32 +1,7 @@
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs'
-import config from './build.config'
+import { compiler } from './build.config'
 
 
-export default [{
+export default compiler({
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-  },
-  ...config,
-
-  plugins: [
-    json(),
-    resolve(),
-    babel({
-      babelrc: false,
-      presets: [
-        ["env", { modules: false }],
-      ],
-      plugins: ["transform-object-rest-spread", "external-helpers"],
-      // runtimeHelpers: true,
-      exclude: 'node_modules/**',
-    }),
-    commonjs({
-      include: 'node_modules/**',
-    }),
-  ],
-}]
+  output: { file: 'dist/bundle.js' },
+})
