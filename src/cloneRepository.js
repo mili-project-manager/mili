01b8fed359de2d71516d5f6857761e7ee7b2a3f8
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-10-21 20:44:33 +0800

    feat: add update command
    resolve #16

--- src/cloneRepository.js
+++ src/cloneRepository.js
@@ -3,21 +3,44 @@ const git = require('simple-git/promise')
 const { join } = require('path')
 const paths = require('./paths')
 const { promisify } = require('util')
+const throwError = require('./throwError')
 
 
 const access = promisify(fs.access)
 
 
-module.exports = async repository => {
+module.exports = async (repository, version) => {
   const templatePath = join(paths.templates, repository)
 
   try {
     await access(templatePath, fs.constants.F_OK)
     await git(templatePath).pull()
-    // pull new commit
   } catch(err) {
+    // BUG: need delete folder
     await git().clone(repository, templatePath)
   }
 
-  return templatePath
+  const gitT = git(templatePath)
+  const tags = await gitT.tags()
+
+  const branchSummary = await gitT.branch()
+  const currentBranch = branchSummary.current
+
+
+  if (version) {
+    version = `v${version}`
+    if (!tags.all.includes(version)) {
+      throwError([
+        'No corresponding template version was found',
+        'Please confirm that the version number exists in the tags of the template repository.',
+        `Expected template version: ${version}`
+      ].join('\n'))
+    }
+
+    gitT.checkout(version)
+  } else if (tags.latest) {
+    await gitT.checkout(tags.latest)
+  }
+
+  return { templatePath, currentBranch }
 }
--- src/loadMiliConfig.js
+++ src/loadMiliConfig.js
@@ -10,7 +10,12 @@ module.exports = async () => {
   if (result) config = result.config || {}
 
 
-  if (!config.version || !semver.valid(config.version)) throwError([
+  if (!config.version) throwError([
+    'Unable to get template version from .milirc',
+    'Please check if the .milirc file is configured correctly.',
+  ].join('\n'))
+
+  if (!semver.valid(config.version)) throwError([
     'The version number of the template finded in .milirc does not conform to the semver specification',
     'Please check if the .milirc file is configured correctly.',
   ].join('\n'))
