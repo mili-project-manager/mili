2c16b3c4656cef04e500df1822ec5694ae0610e4
Val.istar.Guo <val.istar.guo@gmail.com>
2018-11-03 14:54:06 +0800

    fix: extract unexpect string when cannot find tag

--- src/handlers/extractArea.js
+++ src/handlers/extractArea.js
@@ -6,15 +6,17 @@ const readTargetFile = require('./readTargetFile')
 module.exports = (name, begin, end = begin) => createHandler(file => {
   file = readTargetFile(file)
 
-  if (file.targetFile.exist) {
-    let beginIndex = file.targetFile.content.indexOf(begin)
-    beginIndex += begin.length
-    const endIndex = file.targetFile.content.indexOf(end, beginIndex)
+  if (!file.targetFile.exist) return file
 
-    if (beginIndex && endIndex) {
-      file.view.custom[name] = file.targetFile.content.substring(beginIndex, endIndex)
-    }
-  }
+  let beginIndex = file.targetFile.content.indexOf(begin)
+  if (beginIndex === -1) return file
+
+  beginIndex += begin.length
+  const endIndex = file.targetFile.content.indexOf(end, beginIndex)
+
+  if (endIndex === -1) return file
+
+  file.view.custom[name] = file.targetFile.content.substring(beginIndex, endIndex)
 
   return file
 })
