dd4d6cc30f262d43b041ba386b6b2294d6aba283
Val.istar.Guo <val.istar.guo@gmail.com>
2018-11-06 22:42:31 +0800

    chore: check if the working directory is clean

--- src/statusCheck.js
+++ src/statusCheck.js
@@ -14,13 +14,20 @@ const reminder = [
   'This command may cause some files to be overwritten',
   "If you're sure you want to run this command, rerun it with --force.",
 ].join('\n')
+
+const isWorkDirClean = async path => {
+  const { files } =  await git(path).status()
+
+  return !files.length
+}
+
 module.exports = async path => {
-  if (!(await isEmpty(path))) {
+  if (!(await git(path).checkIsRepo()) && !(await isEmpty(path))) {
     throwError([
       'The project directory is not a git repository and is a non-empty folder.',
       reminder,
     ].join('\n'))
-  } else if (await git(path).checkIsRepo()) {
+  } else if (!(await isWorkDirClean(path))) {
     throwError([
       'Git working directory not clean',
       reminder,
