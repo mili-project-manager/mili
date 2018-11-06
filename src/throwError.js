955d87a3045b66b087a5de2ab84148a4748e70c7
Val.istar.Guo <val.istar.guo@gmail.com>
2018-11-06 22:48:56 +0800

    style: optimize error log

--- src/throwError.js
+++ src/throwError.js
@@ -1,7 +1,6 @@
 module.exports = message => {
   throw new Error([
     message,
-    'please confirm if the loaded template supports the current mili version,',
-    'and feedback this question to the template developer.',
+    'If this is an error caused by the template, please feedback this question to template developer.',
   ].join('\n'))
 }
