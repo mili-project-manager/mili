c6fb9aca30372a75b99c99659eb873833cab6351
Val.istar.Guo <val.istar.guo@gmail.com>
2019-04-03 13:11:52 +0800

    feat(handlers): add ejs handler

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -2,6 +2,7 @@ const merge = require('./merge')
 const exist = require('./exist')
 const mustache = require('./mustache')
 const extractArea = require('./extract-area')
+const ejs = require('./ejs')
 
 
-module.exports = { merge, mustache, extractArea, exist }
+module.exports = { merge, mustache, ejs, extractArea, exist }
