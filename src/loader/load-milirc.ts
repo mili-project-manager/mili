56edb92fc70480a698a1f6c70cb5023b8e05a5a2
Val.istar.Guo <val.istar.guo@gmail.com>
2019-07-05 09:56:51 +0800

    fix: mili@1 error should before config valiator

--- src/loader/load-milirc.ts
+++ src/loader/load-milirc.ts
@@ -27,11 +27,13 @@ export default async(cwd: string): Promise<Milirc | null> => {
   const config = clone(result.config)
   if (!config) throw new Error('Cannot load .milirc.yml')
 
+  /* Should before validate. mili@1 config won't pass validator */
+  if (config.mili && semver.lt(config.mili.version, '2.0.0')) throw new Error('Never support auto upgrade from mili@1')
+
   const valid = validate(config)
   if (!valid) throw new Error(ajv.errorsText(validate.errors, { dataVar: 'milirc' }))
   const milirc: Milirc = config as Milirc
 
-  if (semver.lt(milirc.mili.version, '2.0.0')) throw new Error('Never support auto upgrade from mili@1')
 
   /** The relative template path saved in .milirc is relative to the dir of .milirc */
   if (isRelativePath(config.template.repository)) {
