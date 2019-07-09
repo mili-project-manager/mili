bab67bb41daf35cd709c5e6cb68f00213317f93f
Val.istar.Guo <val.istar.guo@gmail.com>
2019-07-09 15:32:54 +0800

    feat(handler): support merge .babelrc

--- src/handlers/merge/index.ts
+++ src/handlers/merge/index.ts
@@ -3,11 +3,13 @@ import { FileGenerator, Handler } from '@/internal'
 import mergeJson from './merge-json'
 import mergeIgnore from './merge-ignore'
 import mergeYaml from './merge-yaml'
+import mergeBabelrc from './merge-babel'
 
 
 const jsonFileExts = ['.json']
 const yamlFileExts = ['.yaml', '.yml']
 const ignoreFilenames = ['.gitignore', '.npmignore', '.npmrc']
+const babelrcFilenames = ['.babelrc']
 
 const notSupportErrorMessage = (filename, projectPath): string => [
   `Merge files of this type(${filename}) are not supported by merge handler.`,
@@ -25,6 +27,7 @@ const genFile: FileGenerator = async(file, resource) => {
   if (jsonFileExts.includes(ext)) await mergeJson(file, resource)
   else if (yamlFileExts.includes(ext)) await mergeYaml(file, resource)
   else if (ignoreFilenames.includes(filename)) await mergeIgnore(file, resource)
+  else if (babelrcFilenames.includes(filename)) await mergeBabelrc(file, resource)
   else throw new Error(notSupportErrorMessage(filename, projectPath))
 }
 
