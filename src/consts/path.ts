59efb0aa46c9a881e9b504f3ea50a1d6588c7551
Val-istar-Guo <val.istar.guo@gmail.com>
2020-04-27 11:06:34 +0800

    feat: cache the templates in users home dir

--- src/consts/path.ts
+++ src/consts/path.ts
@@ -1,4 +1,5 @@
 import { join } from 'path'
+import { homedir } from 'os'
 
 export type Path = Readonly<string>
-export const TEMPLATE_STORAGE: Path = join(__dirname, '../templates')
+export const TEMPLATE_STORAGE: Path = join(homedir(), '.mili', 'templates')
