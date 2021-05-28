a62477321e5edf552f0e984ea5403a40fe0260ee
val.istar.guo <val.istar.guo@gmail.com>
2021-05-28 21:45:57 +0800

    fix: loop copy

--- src/const.ts
+++ src/const.ts
@@ -3,4 +3,4 @@ import * as path from 'path'
 
 export const TEMPLATE_CACHE_FILEPATH = path.join(__dirname, '../template_cache')
 export const TEMPLATE_STORAGE_FILEPATH = path.join(__dirname, '../template_storage')
-export const TEMPORARY_FILEPATH = path.join(__dirname, '../temporary')
+export const TEMPORARY_FILEPATH = path.join('~/.mili', './temporary')
