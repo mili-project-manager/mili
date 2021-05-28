36e53d26f1950c5bf8a4314a1a1357afb5eae38f
val.istar.guo <val.istar.guo@gmail.com>
2021-05-28 13:02:05 +0800

    fix: bad temporary directory path

--- src/util/create-tmp-dir.ts
+++ src/util/create-tmp-dir.ts
@@ -1,11 +1,11 @@
+import { TEMPORARY_FILEPATH } from '@/const'
 import * as fs from 'fs-extra'
 import * as path from 'path'
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuid } from 'uuid'
 
 
 export async function createTmpDir(): Promise<string> {
-  const id = uuidv4()
-  const tmpPath = path.join(__dirname, '../../.mili', id)
+  const tmpPath = path.join(TEMPORARY_FILEPATH , uuid())
 
   if (await fs.pathExists(tmpPath)) throw new Error('Folder is used by another process.')
   await fs.ensureDir(tmpPath)
