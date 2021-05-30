3bdf2d96b80bf2c8d05473ad9ab3e85ede766871
val.istar.guo <val.istar.guo@gmail.com>
2021-05-30 18:24:30 +0800

    refactor: move cache to os temp dir

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,7 @@
 import * as path from 'path'
+import * as tempDir from 'temp-dir'
 
 
-export const TEMPLATE_CACHE_FILEPATH = path.join(__dirname, '../template_cache')
-export const TEMPLATE_STORAGE_FILEPATH = path.join(__dirname, '../template_storage')
-export const TEMPORARY_FILEPATH = path.resolve('~/.mili/temporary')
+export const TEMPLATE_CACHE_FILEPATH = path.join(tempDir, 'mili', 'template_cache')
+export const TEMPLATE_STORAGE_FILEPATH = path.join(tempDir, 'mili', 'template_storage')
+export const TEMPORARY_FILEPATH = path.join(tempDir, 'mili', 'temporary')
