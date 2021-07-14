a17f05d0dd44d94e970d0379f2c99c312bdc7007
val.istar.guo <val.istar.guo@gmail.com>
2021-07-15 01:04:32 +0800

    build: unable publish

--- ava.config.js
+++ ava.config.js
@@ -2,5 +2,4 @@ export default {
   files: ['tests/**/*.ts'],
   extensions: ['ts'],
   require: ['ts-node/register'],
-  timeout: '20s',
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from './src/index'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 /** @ts-ignore */
 import defaultExport from './src/index'
 export default defaultExport
