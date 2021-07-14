281ae1db5a4b735cbddbefe53f06db9217013cde
val.istar.guo <val.istar.guo@gmail.com>
2021-07-15 00:28:55 +0800

    test: fix test for parse-template.ts

--- tests/util/parse-template.ts
+++ tests/util/parse-template.ts
@@ -2,20 +2,20 @@ import test from 'ava'
 import { parseTemplate } from '@/util/parse-template'
 
 test('NPM Template', t => {
-  const repository = parseTemplate('npm:@mtpl/code-style', 'latest')
+  const repository = parseTemplate('npm:@mtpl/code-style', 'latest', {})
 
   t.is(repository.name, '@mtpl/code-style')
 })
 
 test('Git Http Template', t => {
-  const repository = parseTemplate('https://github.com/mili-project-manager/mili.git', 'latest')
+  const repository = parseTemplate('https://github.com/mili-project-manager/mili.git', 'latest', {})
 
   t.is(repository.name, 'https://github.com/mili-project-manager/mili.git')
 })
 
 
 test('Git SSH Template', t => {
-  const repository = parseTemplate('git@github.com:mili-project-manager/mili.git', 'latest')
+  const repository = parseTemplate('git@github.com:mili-project-manager/mili.git', 'latest', {})
 
   t.is(repository.name, 'git@github.com:mili-project-manager/mili.git')
 })
